74968477b3b6378a65e9fc3b0d0cf59caa3cdbeb
Jack Toke <jackptoke@gmail.com>
2020-01-16 16:08:25 +1100

    changed model name of host to presenter...it makes more sense and removing the node_modules

---
+++ models/presenter.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+require('dotenv').config();
+
+const presenterSchema = new Schema({
+  first_name: 
+  {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  last_name: 
+  {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  title:{
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  qualification:{
+    type: String,
+    required: true
+  },
+  short_description: {
+      type: String,
+      required: false
+  },
+  long_description:{
+      type: String,
+      required: true
+  },
+  avatar:{
+    type: String,
+    default: 'my_avatar.jpg'
+  }
+},{
+  collection: 'hosts'
+});
+
+const User = mongoose.model("Host", presenterSchema);
+
+module.exports = User;
