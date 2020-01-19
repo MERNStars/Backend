f9067d79b6c226b6249e8d1ed09e1c6012731c06
Jack Toke <jackptoke@gmail.com>
2020-01-19 17:35:13 +1100

    event search by keywords implemented

--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -2,19 +2,25 @@ const Event = require('../models/event');
 
 //create an event
 const createEvent = (req, res) => {
-    const {event_name, description, event_date_and_presenters, fee, is_family_friendly, minimum_age, maximum_capacity, event_category, images} = req.body;
+    const {event_name, description, event_date_and_presenters, registration_closed_date, fee, is_family_friendly, minimum_age, event_category, images, event_capacity, published, status} = req.body;
 
-    const newEvent = new Event({event_name, description, event_date_and_presenters, fee, is_family_friendly, minimum_age, maximum_capacity, event_category, images});
+    const {isAdmin} = req.decoded;
+    //if the user is not an admin, he has no business here.
+    if(!isAdmin){
+        return res.status(401).json({success: false, message: "You don't have the administrative rights to carryout this update."});
+    }
+
+    const newEvent = new Event({event_name, description, event_date_and_presenters, registration_closed_date, fee, is_family_friendly, minimum_age, event_capacity, event_category, images, published, status});
     newEvent.save()
     .then(()=>res.status(201).json(`A new event has been created!`))//return the result
-    .catch(err=> res.status(400).json('Error: ' + err));
+    .catch(err=> res.status(500).json('Error: ' + err));
 
 }
 
 const index = (req, res) => {
     Event.find({}, (err, result) =>{
         if(err){
-            res.status(400).json(err);
+            res.status(500).json(err);
         }
         else{
             res.status(200).json(result);
@@ -24,22 +30,22 @@ const index = (req, res) => {
 }
 
 const update = async (req, res) => {
-    const {_id, event_name, description, event_date_and_presenters, fee, is_family_friendly, minimum_age, maximum_capacity, event_category, images} = req.body;
+    const {_id, event_name, description, event_date_and_presenters, registration_closed_date, fee, is_family_friendly, minimum_age, event_capacity, event_category, images, published, status, attendee_count} = req.body;
 
     const {isAdmin} = req.decoded;
-
+    //if the user is not an admin, he has no business here.
     if(!isAdmin){
-        return res.status(400).json({success: false, message: "You don't have the administrative rights to carryout this update."});
+        return res.status(401).json({success: false, message: "You don't have the administrative rights to carryout this update."});
     }
 
-    res = await Event.updateOne({_id: _id}, {event_name, description, event_date_and_presenters, fee, is_family_friendly, minimum_age, maximum_capacity, event_category, images}, 
+    res = await Event.updateOne({_id: _id}, {event_name, description, event_date_and_presenters, registration_closed_date, fee, is_family_friendly, minimum_age, event_category, images, published, status, event_capacity, attendee_count}, 
         (err, result) =>{
         if(err){
-            res.status(400).json(err)
+            res.status(500).json(err)
         }
         
         if(result.n > 0)
-            res.status(200).json(result);
+            res.status(204).json(result);
         else
             res.status(400).json(result);
     })
@@ -47,4 +53,52 @@ const update = async (req, res) => {
     return res;
 }
 
-module.exports = { createEvent, index, update }
+const deleteEvent = async (req, res) => {
+    const {_id} = req.body;
+
+    const {isAdmin} = req.decoded;
+    //if the user is not an admin, he has no business here.
+    if(!isAdmin){
+        return res.status(500).json({success: false, message: "You don't have the administrative rights to carryout this update."});
+    }
+
+    res = await Event.findByIdAndDelete(_id, 
+        (err, result) =>{
+        if(err){
+            res.status(400).json(err)
+        }
+        console.log(result);
+        
+        // if(result.n > 0)
+            res.status(200).json(result);
+        // else
+        //     res.status(400).json(result);
+    })
+    .catch(err => res.status(400).json(err));
+    return res;
+}
+
+const findEventByKeywords = (req, res) => {
+    const {keywords} = req.params;
+    
+    //break up the keywords and form a series of conditions for the queiry
+    const conditions1 = keywords.split(' ').map(keyword => { 
+        return JSON.parse(`{"event_name": { "$regex": "${keyword}", "$options": "i" }}`);
+    });
+    const conditions2 = keywords.split(' ').map(keyword => { 
+        return JSON.parse(`{"event_category": { "$regex": "${keyword}", "$options": "i" }}`);
+    });
+    let conditions3 = (keywords.split(' ').map(keyword => { 
+        return JSON.parse(`{"description": { "$regex": "${keyword}", "$options": "i" }}`);
+    }));
+    
+    const conditions = [...conditions1, ...conditions2, ...conditions3]
+
+    console.log(conditions);
+
+    Event.find( { $or: conditions })
+    .then((events)=> res.status(200).json(events))
+    .catch(err => res.status(400).json({success: false, message: `An error has occured.`}));
+}
+
+module.exports = { createEvent, index, update, deleteEvent, findEventByKeywords }
--- routes/event_routes.js
+++ routes/event_routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const middleware = require('./token_middleware');
 
-const {createEvent, index, update} = require('../controllers/event_controllers');
+const {createEvent, index, update, deleteEvent, findEventByKeywords} = require('../controllers/event_controllers');
 
 router.get('/', index);
+router.get('/:keywords', findEventByKeywords);
 router.post('/create', middleware.checkAdminToken, createEvent);
 router.patch('/update', middleware.checkAdminToken, update);
+router.delete('/delete', middleware.checkAdminToken, deleteEvent);
 
 module.exports = router;
