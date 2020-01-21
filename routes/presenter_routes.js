38680107b3129ce47c1a06be7eae9eea61bc683d
Jack Toke <jackptoke@gmail.com>
2020-01-21 15:34:08 +1100

    No longer need authentication to see presenters

--- routes/presenter_routes.js
+++ routes/presenter_routes.js
@@ -4,8 +4,8 @@ const middleware = require('./token_middleware');
 
 const { index, createPresenter, updatePresenter, deletePresenter, findPresenterByName, findPresenterById } = require("../controllers/presenter_controllers");
 
-router.get('/', middleware.checkAdminToken, index);
-router.get('/:id', middleware.checkAdminToken, findPresenterById);
+router.get('/', index);
+router.get('/:id', findPresenterById);
 router.post('/create', middleware.checkAdminToken, createPresenter);
 router.patch('/update', middleware.checkAdminToken, updatePresenter);
 router.delete('/delete', middleware.checkAdminToken, deletePresenter);
