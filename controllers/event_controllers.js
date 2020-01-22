dee34e3949045edffa56aa226388d97e9803f43b
Jack Toke <jackptoke@gmail.com>
2020-01-22 13:56:28 +1100

    Added attend and attend routes for events

--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -130,7 +130,7 @@ const findEventById = (req, res) => {
             const event_detail = {...event}
             if(presenters)
                 event_detail.presenter_detail = presenters;
-            console.log(event_detail);
+            // console.log(event_detail);
             res.send(event_detail);
         })
     })
@@ -138,23 +138,90 @@ const findEventById = (req, res) => {
         console.log('error', err);
         res.status(400).send(err);
     });
-
-
-
-    //     },
-    //     (event, done)=>{
-    //         Presenter.find({ _id: { $in: event.presenters}}).lean.exec(done);
-    //     }
-    // ], (err, presenter)=>{
-    //     // if an error occurs during the above tasks ^, it will come down here
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(400).send(e);
-    //     }
-    //     // otherwise if all tasks finish, it will come down here
-    //     res.json(presenter);
-    // });
     return res;
 }
 
-module.exports = { createEvent, index, update, deleteEvent, findEventByKeywords, findEventById, findEventCategory }
+const attendEvent = async (req, res) => {
+    const {_id, username, friends, dependents} = req.body;
+    const new_attendee = {username, friends, dependents};
+
+    const filter = { "_id": _id};
+
+    Event.findOne({ _id: _id}, (err, event) => {
+        console.log("Event found: " + event);
+        if(err || event == null || event == undefined){
+            return res.status(500).json({success: false, message: "Can't find the event."});
+        }
+        const existing_attendees = [...event.attendees];
+        const foundData = existing_attendees.find(attendee =>{
+            return attendee.username === username;
+        });
+        console.log("Attendee search outcome: " + foundData);
+        if(!foundData){
+            const num_attendee = event.attendee_count + friends.length + dependents.length + 1;
+
+            existing_attendees.push(new_attendee);//update the attendees array
+            Event.updateOne(filter, {attendees: existing_attendees, attendee_count: num_attendee}, 
+                (err, result) =>{
+                if(err){
+                    res.status(500).json(err)
+                }
+                console.log("Update result: " + result);
+                if(result.n > 0)
+                    res.status(200).json({success: true, message: "Successfully added an attendee."});
+                else
+                    res.status(400).json({success: false, message: "Failed to add attendee."});
+            });
+        }
+        else{
+            res.status(400).json({success: false, message: "Ahhh!  You have already signed up to attend the event."});
+        }
+        return event;
+    })
+    .catch(err => res.status(400).json({success: false, message: "Ooops!  Something went terribly wrong."}));
+    
+    return res;
+}
+
+const unattendEvent = async (req, res) => {
+    const {_id, username} = req.body;
+
+    const filter = { "_id": _id};
+
+    Event.findOne({ _id: _id}, (err, event) => {
+        console.log("Event found: " + event);
+        if(err || event == null || event == undefined){
+            return res.status(500).json({success: false, message: "Can't find the event."});
+        }
+        const existing_attendees = [...event.attendees];
+        const foundAt = existing_attendees.findIndex(attendee =>{
+            return attendee.username === username;
+        });
+        console.log("Attendee search outcome: " + foundAt);
+        if(foundAt >= 0){
+            const num_attendee = event.attendee_count - event.attendees[foundAt].friends.length - event.attendees[foundAt].dependents.length - 1;
+
+            existing_attendees.splice(foundAt, 1);//update the attendees array
+            Event.updateOne(filter, {attendees: existing_attendees, attendee_count: num_attendee}, 
+                (err, result) =>{
+                if(err){
+                    res.status(500).json({success: false, message: "Something is not right."})
+                }
+                console.log("Update result: " + result);
+                if(result.n > 0)
+                    res.status(200).json({success: true, message: "Successfully removed an attendee."});
+                else
+                    res.status(400).json({success: false, message: "Failed to remove attendee."});
+            });
+        }
+        else{
+            res.status(400).json({success: false, message: "Ayyy!  You have not signed up to attend the event."});
+        }
+        return event;
+    })
+    .catch(err => res.status(400).json({success: false, message: "Ooops!  Something went terribly wrong."}));
+    
+    return res;
+}
+
+module.exports = { createEvent, index, update, deleteEvent, findEventByKeywords, findEventById, findEventCategory, attendEvent, unattendEvent }
