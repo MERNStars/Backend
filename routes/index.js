e7c8b87ca928cc0b6356d8476bf5086a710284d0
Jack Toke <jackptoke@gmail.com>
2020-01-23 15:48:51 +1100

    Backend aws image url request implemented

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ router.use("/users", require("./user_routes"));
 router.use("/presenters", require("./presenter_routes"));
 router.use("/events", require("./event_routes"));
 router.use("/email", require('./email_routes'));
+router.use('/image', require('./image_routes'));
 // router.get("/", require("../controllers/index"));
 
 module.exports = router;
--- utilities/aws-connection.js
+++ utilities/aws-connection.js
@@ -1,14 +1,22 @@
+const { uuid } = require('uuidv4');
+var aws = require('aws-sdk');
+
+aws.config.update({
+    accessKeyId: process.env.AWS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+});
+
 async function getPresignedUploadUrl(bucket, directory) {
-  const key = `${directory}/${uuid.v4()}`;
-  const url = await s3
-    .getSignedUrl('putObject', {
+  const key = `${directory}/${uuid()}`;
+  var s3 = new aws.S3();
+  const url = await s3.getSignedUrl('putObject', {
       Bucket: bucket,
       Key: key,
       ContentType: 'image/*',
       Expires: 300,
     })
     .promise();
-  return url;
+return url;
 }
 
 
